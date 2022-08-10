09cdbb83aa0dd83a77bfa80fad68cf6ee0db0afb
LukaFilipovic99 <74373893+LukaFilipovic99@users.noreply.github.com>
2022-08-10 02:16:36 +0200

    Added navbar and HomeFrame

---
+++ src/com/lukafilipovic/AlfaRomeoCarConfigurator/view/HomeFrame.java
@@ -0,0 +1,74 @@
+package com.lukafilipovic.AlfaRomeoCarConfigurator.view;
+
+import com.lukafilipovic.AlfaRomeoCarConfigurator.model.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.swing.*;
+import java.awt.*;
+
+@Getter
+@Setter
+public class HomeFrame extends JFrame {
+    private JLabel configureNewCarLbl;
+    private JButton configureNewCarBtn;
+    private JLabel viewConfiguredCarsLbl;
+    private JButton viewConfiguredCarsBtn;
+    private JPanel selectPanel;
+    private JPanel navPanel;
+
+    public HomeFrame(){
+        super("Alfa Romeo Konfigurator");
+        setSize(700, 400);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        initComps();
+        initSelectPanelLayout();
+        setLayout(new BorderLayout());
+        add(navPanel,BorderLayout.NORTH);
+        add(selectPanel, BorderLayout.CENTER);
+        setVisible(true);
+    }
+
+    private void initComps(){
+        Font font=new Font("Arial", Font.PLAIN, 20);
+        Font fontBtn=new Font("Arial", Font.BOLD, 30);
+        configureNewCarLbl=new JLabel("<html><p>Konfigurirajte vaš<br>novi Alfa Romeo</p></html>");
+        configureNewCarLbl.setFont(font);
+        configureNewCarBtn=new JButton(">");
+        configureNewCarBtn.setFont(fontBtn);
+        configureNewCarBtn.setBackground(Color.DARK_GRAY);
+        configureNewCarBtn.setForeground(Color.WHITE);
+        viewConfiguredCarsLbl= new JLabel("<html><p>Pregledajte već<br>konfigurirane automobile</p></html>");
+        viewConfiguredCarsLbl.setFont(font);
+        viewConfiguredCarsBtn=new JButton(">");
+        viewConfiguredCarsBtn.setFont(fontBtn);
+        viewConfiguredCarsBtn.setBackground(Color.DARK_GRAY);
+        viewConfiguredCarsBtn.setForeground(Color.WHITE);
+        selectPanel=new JPanel();
+        navPanel=new NavPanel();
+    }
+
+    private void initSelectPanelLayout(){
+        selectPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(20, 30, 0, 30);
+        gbc.anchor = GridBagConstraints.CENTER;
+        selectPanel.add(configureNewCarLbl, gbc);
+        gbc.gridx = 1;
+        gbc.anchor = GridBagConstraints.CENTER;
+        selectPanel.add(viewConfiguredCarsLbl, gbc);
+
+        gbc.gridx=0;
+        gbc.gridy=1;
+        selectPanel.add(configureNewCarBtn, gbc);
+        gbc.gridx=1;
+        selectPanel.add(viewConfiguredCarsBtn,gbc);
+    }
+
+
+}
---
+++ src/com/lukafilipovic/AlfaRomeoCarConfigurator/view/NavPanel.java
@@ -0,0 +1,48 @@
+package com.lukafilipovic.AlfaRomeoCarConfigurator.view;
+
+import com.lukafilipovic.AlfaRomeoCarConfigurator.model.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.swing.*;
+import java.awt.*;
+
+@Getter
+@Setter
+public class NavPanel extends JPanel{
+    private JButton logOutBtn;
+    private JButton backToHomepageBtn;
+    private JLabel userName;
+    private User user;
+
+    public NavPanel(){
+        initComps();
+        initLayout();
+        setVisible(true);
+    }
+
+    private void initComps(){
+        setBackground(Color.DARK_GRAY);
+        Font font=new Font("Arial", Font.PLAIN, 20);
+        Font fontBtn=new Font("Arial", Font.BOLD, 20);
+        logOutBtn=new JButton("Odjavi se");
+        logOutBtn.setFont(fontBtn);
+        logOutBtn.setBackground(Color.GRAY);
+        logOutBtn.setForeground(Color.WHITE);
+        backToHomepageBtn=new JButton("Početna");
+        backToHomepageBtn.setFont(fontBtn);
+        backToHomepageBtn.setBackground(Color.GRAY);
+        backToHomepageBtn.setForeground(Color.WHITE);
+        userName=new JLabel("Luka Filipović");
+        userName.setFont(font);
+        userName.setForeground(Color.WHITE);
+    }
+
+    private void initLayout() {
+
+        setLayout(new FlowLayout());
+        add(userName);
+        add(backToHomepageBtn);
+        add(logOutBtn);
+    }
+}
